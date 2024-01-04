f200b816628045bc748898447a32d1b89680d075
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-03 23:18:57 -0300

    add codigos das classes do pacote DataBase

--- src/main/java/com/minhaempresa/infraestructure/database/DataBaseException.java
+++ src/main/java/com/minhaempresa/infraestructure/database/DataBaseException.java
@@ -1,4 +1,13 @@
 package com.minhaempresa.infraestructure.database;
 
-public class DataBaseException {
+/*
+    A classe abaixo foi derivada da RuntimeException para que no programa não precise toda hora
+    colocando o try...catch
+ */
+public class DataBaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DataBaseException(String mensagem) {
+        super(mensagem);
+    }
 }
--- src/main/java/com/minhaempresa/infraestructure/database/DataBaseIntegrityException.java
+++ src/main/java/com/minhaempresa/infraestructure/database/DataBaseIntegrityException.java
@@ -1,4 +1,9 @@
 package com.minhaempresa.infraestructure.database;
 
-public class DataBaseIntegrityException {
+public class DataBaseIntegrityException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DataBaseIntegrityException(String msg) {
+        super(msg);
+    }
 }
