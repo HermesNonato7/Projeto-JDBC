36f8668bf0a0d1ad33c504d79352e0996a9a970c
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-06 21:24:27 -0300

    add comentario DataBaseIntegrityException

--- src/main/java/com/minhaempresa/infrastructure/database/DataBaseIntegrityException.java
+++ src/main/java/com/minhaempresa/infrastructure/database/DataBaseIntegrityException.java
@@ -1,5 +1,10 @@
 package com.minhaempresa.infrastructure.database;
 
+/*
+    Essa classe mantém a integridade do banco de dados, impedido
+    que seja apagdo registro de pedido já realizado.
+ */
+
 public class DataBaseIntegrityException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
